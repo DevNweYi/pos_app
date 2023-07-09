3d2e695ac4101a0d9402ca84bf19b89b6f6233c7
DevNweYi <nweyiaung610@gmail.com>
2023-07-09 22:18:43 +0630

    implemented drop down list

--- lib/create_item_page.dart
+++ lib/create_item_page.dart
@@ -15,26 +15,19 @@ class CreateItemPage extends StatefulWidget {
 }
 
 class _CreateItemPageState extends State<CreateItemPage> {
-
-  var itemController=Get.put(ItemController());
-  List<CategoryData> lstCategory=[];
-  String dropdownvalue="";
-  /* var categories = [
-    'Category 1',
-    'Category 2',
-    'Category 3',
-    'Category 4',
-    'Category 5',
-  ]; */
+  var itemController = Get.put(ItemController());
+  List<CategoryData> lstCategory = [];
+  CategoryData dropdownvalue =
+      CategoryData(categoryId: 0, categoryCode: "", categoryName: "");
 
   @override
   void initState() {
     DatabaseHelper().getCategory().then((value) {
       setState(() {
-        lstCategory=value;
-        if(lstCategory.isNotEmpty){
-          dropdownvalue=lstCategory[0].categoryName;
+        if (value.isNotEmpty) {
+          dropdownvalue = value[0];
         }
+        lstCategory = value;
       });
     });
     super.initState();
@@ -43,16 +36,17 @@ class _CreateItemPageState extends State<CreateItemPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: const Text(AppString.createItem),
         backgroundColor: AppColor.primary,
         foregroundColor: Colors.black87,
         actions: [
           TextButton(
-            child: const Text(AppString.save,style: TextStyle(color: Colors.black87),),
-            onPressed: () {
-              
-            },
+            child: const Text(
+              AppString.save,
+              style: TextStyle(color: Colors.black87),
+            ),
+            onPressed: () {},
           )
         ],
       ),
@@ -61,122 +55,130 @@ class _CreateItemPageState extends State<CreateItemPage> {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-                Container(               
-                  decoration: BoxDecoration(border: Border.all(color: Colors.black12),borderRadius: const BorderRadius.all(Radius.circular(5))),
-                  child: TextField(
-                    controller: itemController.codeController,
-                    textInputAction: TextInputAction.next,
-                    decoration: const InputDecoration(
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black12),
+                    borderRadius: const BorderRadius.all(Radius.circular(5))),
+                child: TextField(
+                  controller: itemController.codeController,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
                       hintText: AppString.code,
-                      border: OutlineInputBorder(borderSide: BorderSide.none)
-                    ),               
-                  ),
-                ),              
-                Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  decoration: BoxDecoration(border: Border.all(color: Colors.black12),borderRadius: const BorderRadius.all(Radius.circular(5))),
-                  child: TextField(
-                    controller: itemController.nameController,
-                    textInputAction: TextInputAction.next,
-                    decoration: const InputDecoration(
-                      hintText: AppString.name,
-                      border: OutlineInputBorder(borderSide: BorderSide.none)
-                    ),               
-                  ),
+                      border: OutlineInputBorder(borderSide: BorderSide.none)),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  decoration:
-                      BoxDecoration(border: Border.all(color: Colors.black12),borderRadius: const BorderRadius.all(Radius.circular(5))),
-                  height: 60,
-                  child: DropdownButtonHideUnderline(
-                    child: ButtonTheme(
-                      alignedDropdown: true,
-                      child: DropdownButton(
-                        value: dropdownvalue,
-                        icon: const Icon(Icons.keyboard_arrow_down),
-                        items: lstCategory.map((e){
-                          return DropdownMenuItem(
-                              value: e.categoryId,
-                              child: Text(e.categoryName),
-                          );
-                        }).toList(),
-                        /* categories.map((String category) {
-                          return DropdownMenuItem(
-                            value: category,
-                            child: Text(category),
-                          );
-                        }).toList(), */
-                        onChanged: (value) {
-                          setState(() {
-                            dropdownvalue = value.toString();
-                          });
-                        },
-                        isExpanded: true,
-                      ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black12),
+                    borderRadius: const BorderRadius.all(Radius.circular(5))),
+                child: TextField(
+                  controller: itemController.nameController,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                      hintText: AppString.name,
+                      border: OutlineInputBorder(borderSide: BorderSide.none)),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black12),
+                    borderRadius: const BorderRadius.all(Radius.circular(5))),
+                height: 60,
+                child: DropdownButtonHideUnderline(
+                  child: ButtonTheme(
+                    alignedDropdown: true,
+                    child: DropdownButton(
+                      value: dropdownvalue,
+                      icon: const Icon(Icons.keyboard_arrow_down),
+                      items: lstCategory.map((e) {
+                        return DropdownMenuItem<CategoryData>(
+                          value: e,
+                          child: Text(e.categoryName),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          dropdownvalue = value!;
+                        });
+                      },
+                      isExpanded: true,
                     ),
                   ),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  decoration: BoxDecoration(border: Border.all(color: Colors.black12),borderRadius: const BorderRadius.all(Radius.circular(5))),
-                  child: TextField(
-                    controller: itemController.salePriceController,
-                    textInputAction: TextInputAction.next,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black12),
+                    borderRadius: const BorderRadius.all(Radius.circular(5))),
+                child: TextField(
+                  controller: itemController.salePriceController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
                       hintText: AppString.salePrice,
-                      border: OutlineInputBorder(borderSide: BorderSide.none)
-                    ),               
-                  ),
+                      border: OutlineInputBorder(borderSide: BorderSide.none)),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  decoration: BoxDecoration(border: Border.all(color: Colors.black12),borderRadius: const BorderRadius.all(Radius.circular(5))),
-                  child: TextField(
-                    controller: itemController.purchasePriceController,
-                    textInputAction: TextInputAction.next,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black12),
+                    borderRadius: const BorderRadius.all(Radius.circular(5))),
+                child: TextField(
+                  controller: itemController.purchasePriceController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
                       hintText: AppString.purchasePrice,
-                      border: OutlineInputBorder(borderSide: BorderSide.none)
-                    ),               
-                  ),
+                      border: OutlineInputBorder(borderSide: BorderSide.none)),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  decoration: BoxDecoration(border: Border.all(color: Colors.black12),borderRadius: const BorderRadius.all(Radius.circular(5))),
-                  child: TextField(
-                    controller: itemController.costController,
-                    textInputAction: TextInputAction.done,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black12),
+                    borderRadius: const BorderRadius.all(Radius.circular(5))),
+                child: TextField(
+                  controller: itemController.costController,
+                  textInputAction: TextInputAction.done,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
                       hintText: AppString.cost,
-                      border: OutlineInputBorder(borderSide: BorderSide.none)
-                    ),               
-                  ),
+                      border: OutlineInputBorder(borderSide: BorderSide.none)),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      //Image.asset(""),
-                      Container(
-                        color: Colors.red,
-                        height: 100,
-                        width: 100,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          TextButton.icon(onPressed: (){}, icon: const Icon(Icons.photo), label: const Text(AppString.choosePhoto)),
-                          TextButton.icon(onPressed: (){}, icon: const Icon(Icons.photo_camera), label: const Text(AppString.takePhoto)),
-                        ],
-                      )
-                    ],
-                  ),
-                )
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    //Image.asset(""),
+                    Container(
+                      color: Colors.red,
+                      height: 100,
+                      width: 100,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextButton.icon(
+                            onPressed: () {
+                              
+                            },
+                            icon: const Icon(Icons.photo),
+                            label: const Text(AppString.choosePhoto)),
+                        TextButton.icon(
+                            onPressed: () {},
+                            icon: const Icon(Icons.photo_camera),
+                            label: const Text(AppString.takePhoto)),
+                      ],
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
